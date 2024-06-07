dd0b1695465a3a48afe9927ca1a8fe5aeaced5c8
HemangMahajani <76701613+HemangMahajani@users.noreply.github.com>
2024-06-07 19:23:16 +0530

    adding cucumber and TestNG project

---
+++ TestNGMaven/src/test/java/TestNGPackage1/FirstClass.java
@@ -0,0 +1,15 @@
+package TestNGPackage1;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+@Test
+	public void loginTest()
+	
+	{
+		System.out.println("Login is successful");
+		
+	}
+	
+}
